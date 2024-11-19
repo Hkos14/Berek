9643b8c03c2a18f9f434a9c09393946f667db168
Hkos14 <homolyaakos14@gmail.com>
2024-11-19 10:40:35 +0100

    Add project files.

---
+++ Berek/Dolgozok.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Berek
+{
+    internal class Dolgozok
+    {
+        public string Nev { get; set; }
+        public string Neme { get; set; }
+        public string Reszleg { get; set; }
+        public int BelepEv { get; set; }
+        public int Fizetes { get; set; }
+
+        public Dolgozok(string sor)
+        {
+            var adat = sor.Split(';');
+            Nev = adat[0];
+            Neme = adat[1];
+            Reszleg = adat[2];
+            BelepEv = int.Parse(adat[3]);
+            Fizetes = int.Parse(adat[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"Nev:{Nev} \n "+
+                   $"Neme:{Neme} \n "+
+                   $"Részleg: {Reszleg}  \n "+
+                   $"Belépés Éve: {BelepEv}  \n "+
+                   $"Fizetése: {Fizetes} Ft \n";
+        }
+    }
+}
---
+++ Berek/Program.cs
@@ -0,0 +1,45 @@
+﻿using Berek;
+using System;
+using System.Linq;
+using System.Threading.Channels;
+
+List<Dolgozok> dolgozo = new List<Dolgozok>();
+using StreamReader sr = new StreamReader(@"..\..\..\src\berek2020.txt");
+
+_ = sr.ReadLine();
+
+
+while (!sr.EndOfStream) dolgozo.Add(new(sr.ReadLine()));
+
+
+//kezdés 9:55 vége 10:36!!
+
+//3 
+Console.WriteLine($"1.feladat: {dolgozo.Count} db");
+
+//4
+var berek2020 = dolgozo.Average(x => x.Fizetes);
+Console.WriteLine($"2. feladat: {berek2020:0000.0}forint");
+
+//5
+Console.WriteLine("Adjon meg egy részleg nevet:");
+string reszlegtarolas = Console.ReadLine();
+
+//6 
+var reszlegek = dolgozo
+    .Where(x => x.Reszleg.Equals(reszlegtarolas))
+    .OrderByDescending(x => x.Fizetes)
+    .FirstOrDefault(); 
+
+Console.WriteLine(reszlegek != null
+    ? $"6. feladat: {reszlegek.ToString()}"
+    : "6. feladat: A megadott részleg nem létezik a cégnél!");
+
+//7
+var statisztika = dolgozo.GroupBy(x => x.Reszleg);
+foreach (var csoport in statisztika)
+{
+    Console.WriteLine($"Részleg: {csoport.Key}, Dolgozók száma: {csoport.Count()}");
+}
+
+//kezdés 9:55 vége 10:36!!
